da4afeeb36766f59f0abac0291ecc8548d12b0aa
tzkrak <t.zajac@interia.pl>
2018-05-28 18:25:33 +0200

    marge concurency_conflict

--- TestHello.java
+++ TestHello.java
@@ -2,24 +2,41 @@ import java.util.*;
 
 public class TestHello {
 	static String HELLO_MSG = "Hello\n";
+<<<<<<< Updated upstream
 	private Vector<String> stringVector;
+=======
+	private Vector<String> strVect;
+>>>>>>> Stashed changes
 
 	public static void main(String args[]){
 		System.out.println(HELLO_MSG);
 	}
 	
 	public TestHello(){
+<<<<<<< Updated upstream
 		// Tak tez moza ale lepiej na
 		// Na insterfejsach programowac
 		stringVector =  new Vector();
+=======
+		strVect =  new Vector();
+>>>>>>> Stashed changes
 	}
 
 	public String getName(){
 		return HELLO_MSG;
 	}
 
+	public String getName(){
+	public String getName(){
+	public String getName(){
+	public String getName(){
+
+	public String getName(){
+		return HELLO_MSG;
+	}
+
 	public Vector getVector(){
-		return stringVector;
+		return strVect;
 	};
 
 	public int getEage(){
