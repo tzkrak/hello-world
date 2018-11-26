6b0c400e53d1b3c844b7337bbdd9d1e567250168
tzkrak <t.zajac@interia.pl>
2018-11-26 19:40:36 +0100

    havy poniedzialek

---
+++ 20181126.js
@@ -0,0 +1,75 @@
+function f1(){
+	czas podjac decyzje
+
+}
+
+var zmienna = 1;
+
+// podstawowe opracje
+czytaj baze
+zapisz baze
+zrenderuj strone
+
+var fun = function(){};
+
+// adnotacje json - powtorzyc 
+// do uj-tu sie podpiac i jutro niestety trze bedzie sie wziac za dupe
+
+client -> controler -> serwis -> dao -> db
+
+--
+rest -> 
+    mialem edytowac w sblime
+luzne notki ->
+    eclipse kepler, sublime, node
+    gradle jetty
+    apache tomacat
+
+---
+    telefon:
+	odcisk palca, kopia zapasowa, przywracanie ustewien fabryczych, konto google, konto samsung,
+ 	---
+ 	za duzo wiem (na roszerzaniach plikow pracowac)
+   
+
+---
+	kolejne notki:
+		maven
+		gradle build
+		git rebase git cherryquit
+		(nawet nie jestem w stanie na kilku sie skupic na czym kolwiek)
+lunze mysli
+	kurde Klass.prototype.move = function(gh) {
+		gh.constant('name', value);
+		// alt-/ wstawia templaty kodowe
+	};
+// jestem w hub ramie 
+--- jedno centrum dystrybucji
+---
+slynny kontraoler i wzorze mvc IoC, depency injection, convention over confiuguration
+	- backand restowy mozna tez zrobic ba php - aplikacja konfercje
+---
+programowanie funkcyjne - to jest pochodna telefon	
+konfiguracja glosu
+zaktulizowalem meesengera - ale nie wlaczylem integracji z sms i kontaktmi bo nie wiem o co chodzi
+
+pozastaje roczytanie konfiugracji (uwaga na domyslen reguly contrelery viwee rasolwery wydaje sie ze duza czesc aplikacji na domyslnych ustawieniach jest)
+----
+domyslne reguly - (konfiguracja) -- slynny model mvc --- controler -> serwis dao (db) <---- architekruar i teraz pytanie o renderowanie
+
+KURWA KOLLECJO PRZEGLGADANC ---- JAVA TYLKO TO CO KONIECZNE - (KOFIGURACJE - ADNOTACJE - GENERICY - API mapy i inee)
+
+JAVA EE - TO W GRUNCIE SERWLET RESZTA CHODZI NA RESCIE 
+----
+stosy aplikacyjne i generaoty yooman angular stack generato MEAN ::: node
+ANGULARE - przenisienie rednerowania na strone serwera
+
+----
+https://www.dataart.com.pl/news/maciej-sikora-renderowanie-po-stronie-serwera-z-uzyciem-universal-javascript/
+----
+przeniesienie kodu na strone serwera rednorowania
+// - https://medium.freecodecamp.org/48-answers-on-stack-overflow-to-the-most-popular-angular-questions-52f9eb430ab0
+
+uswam rapozytorim i zrobic porzadki
+
+// sprubuj przegladanac kod - aplikacyjny (redndoreowanie stron wazny punkt) --- grafika jutro proba z musnetem
